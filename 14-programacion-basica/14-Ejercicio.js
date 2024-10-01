cd78aafd1d0b7fb5e76c1559eaaf93962a07dd73
PurpleCodee <enyaglf.2000@gmail.com>
2024-10-01 08:37:44 +0200

    01/10/2024

---
+++ 14-programacion-basica/14-Ejercicio.js
@@ -0,0 +1,21 @@
+/** */
+
+let colores = ['grey', 'red', 'blue', 'green', 'yellow', 'black'];
+
+let palabras = e15(['KK', '27', 'green', 'blue', '23']);
+
+let nuevo = [];
+function e15(palabras){
+
+    for(let i=0; i < colores.length; i++){//recorro colores
+
+        for(let j=0; j < palabras.length; j++){//recorro palabras
+            
+        }
+
+
+        //en nuevo guardo los colores
+    }
+    
+
+}
