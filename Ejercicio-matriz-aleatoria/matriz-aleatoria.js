fdcbe136e796d7639d5fe3d7d5b5e0bd588406de
PurpleCodee <enyaglf.2000@gmail.com>
2024-10-08 11:48:52 +0200

    08/10/2024

--- Ejercicio-matriz-aleatoria/matriz-aleatoria.js
+++ Ejercicio-matriz-aleatoria/matriz-aleatoria.js
@@ -13,7 +13,7 @@ b) Mostrar la matriz por consola en filas y columnas
 
 c) Sumar todos los elementos de la matriz y mostrar por pantalla
 
-d) Aplicar Fiz-Buzz en los elemntos de la matriz y devolver otra matriz con los cambios
+d) Aplicar Fiz-Buzz en los elemntos de la matriz y devolver otra matriz con los cambios si es multiplo de 3fizz, multiplo de 5 buzz, de ambos fizz buzz
 */
 
 function main(){
@@ -45,13 +45,38 @@ function main(){
             sumaElemnetos+=matrizAle[i][j];
         }
     }
+
+     //-----------------------------MUESTRO PRIMERA MATRIZ---------------------------------------
     console.log(mayor);//numero mayor
-    console.log(sumaElemnetos);
+    console.log(sumaElemnetos);//guardo la suma de los elementos
 
      //muestro la tabla de multiplicar
      matrizAle.forEach(function(matrizAle){
-        console.log(matrizAle);
+        console.log(matrizAle.join(" "));
     });   
+    //--------------------------------------------------------------------------------------------
+   
+    for(let i = 0; i < matrizAle; i++){
+        let nuevaMatriz = [];
+
+        for(let j = 0; j < matrizAle; i++){
+            nuevaMatriz = matrizAle[i][j];//guardo los valores originales
+
+    //-------------------------------SIGO DENTRO DEL DOBLE BUCLE---------------------------------------
+            if(nuevaMatriz[i][j] % 0 == 3 && nuevaMatriz[i][j] % 0 == 5 ) console.log("fizz");
+                
+
+            else if(nuevaMatriz[i][j] % 0 == 3) console.log("buzz");
+                
+
+            else if(nuevaMatriz[i][j] % 0 == 5 ) console.log("fizzbuzz");     
+              
+        }
+    }
+
+    nuevaMatriz.forEach(function(nuevaMatriz){
+        console.log(nuevaMatriz.join(" "));
+    });  
 }
 
     
