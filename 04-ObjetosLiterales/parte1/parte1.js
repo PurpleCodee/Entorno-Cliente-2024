4123a8b7f742df8cd01b026ab1a77aa63760f272
PurpleCodee <enyaglf.2000@gmail.com>
2025-01-28 11:19:25 +0100

    costas de trabajo clase

--- 04-ObjetosLiterales/parte1/parte1.js
+++ 04-ObjetosLiterales/parte1/parte1.js
@@ -1,4 +1,4 @@
-window.onload = function(){
+window.onload = function () {
     //------------------CREAR OBJETO TUTOR----------------
     //Un objeto tiene clave: "valor"
 
@@ -20,10 +20,10 @@ window.onload = function(){
     //Creo una lista de objetos asignatura
 
     let listaAsignaturas = [
-        {nombre_asignatura: "Matematicas discreta", curso: 1, horas_totales: 17},
-        {nombre_asignatura: "Estadistica", curso: 1, horas_totales: 17},
-        {nombre_asignatura: "ADA", curso: 2, horas_totales: 20},
-        {nombre_asignatura: "Sistemas inteligentes", curso: 1, horas_totales: 20}
+        { nombre_asignatura: "Matematicas discreta", curso: 1, horas_totales: 17 },
+        { nombre_asignatura: "Estadistica", curso: 1, horas_totales: 17 },
+        { nombre_asignatura: "ADA", curso: 2, horas_totales: 20 },
+        { nombre_asignatura: "Sistemas inteligentes", curso: 1, horas_totales: 20 }
     ];
 
     //-------------------OBJETO ALUMNO--------------------- hago uso de tutor y asignatura
@@ -33,7 +33,7 @@ window.onload = function(){
         ciclo: "Ingieneria e software",
         curso: 1,
         //objeto tutor
-        tutor:{
+        tutor: {
             nombre: "Alejandra",
             edad: 29,
             DNI: '05977433D',
@@ -41,40 +41,46 @@ window.onload = function(){
         },
         //Lista asignaturas
         listaAsignaturas: [
-            {nombre_asignatura: "Sistemas inteligentes", curso: 1, horas_totales: 20}
+            { nombre_asignatura: "Sistemas inteligentes", curso: 1, horas_totales: 20 }
         ],
 
         //Lista de notas medias
         listaNotaMedia: [
             //creo objeto para relacionar asignatura con nota
-            {asignatura:"Sistemas inteligentes", nota_media: 8.3}
-         ]
+            { asignatura: "Sistemas inteligentes", nota_media: 8.3 }
+        ]
 
     };
 
     for (const attribute in alumno) {
-        if (attribute === "tutor"){
-            console.log("Atributos del tutor: ");
-            for (const tutor_attribute in alumno[attribute]){
-                console.log("Atributo tutor " + tutor_attribute + ", valor: " + alumno[attribute][tutor_attribute]);
+        //------------------MUESTRO DATOS DEL ALUMNO---------------------
+        CrearElemento(attribute + ": ", alumno[attribute], "alumno");
+
+        if (attribute === "tutor") {
+            for (const tutor_attribute in alumno[attribute]) {
+                //console.log("Atributo tutor " + tutor_attribute + ", valor: " + alumno[attribute][tutor_attribute]);
+                //------------------MUESTRO DATOS DEL TUTOR---------------------
+                CrearElemento(tutor_attribute + ": ", alumno[attribute][tutor_attribute], "tutor");
             }
-        }else if (attribute === "listaAsignaturas" || attribute === "listaNotaMedia") {
-            for (const array_element in alumno[attribute]){
+        } else if (attribute === "listaAsignaturas" || attribute === "listaNotaMedia") {
+            for (const array_element in alumno[attribute]) {
                 let obj_json = alumno[attribute][array_element]
-                for (const attribute_array_element in obj_json){
+                for (const attribute_array_element in obj_json) {
                     console.log("Atributo " + attribute + " " + attribute_array_element + " , valor: " + obj_json[attribute_array_element])
                 }
             }
-        }else {
+        } else {
             console.log("Attributo " + attribute + ", valor: " + alumno[attribute]);
         }
-        
-        
-        //console.log(alumno[attribute])
     }
 
-    for (const nombre_asignatura in listaAsignaturas) {
-        console.log(nombre_asignatura);
-    }
+    //--------------------------ACEDO A LOS ELEMENTOS DEL HTML-----------------------------------
 
+    //Funcion para crear el texto a los datos.
+    function CrearElemento(mensaje, dato, papa) {
+        let parrafo = document.createElement("p");
+        let divPadre = document.getElementById(papa);
+        parrafo.textContent = mensaje + dato;
+        divPadre.appendChild(parrafo);
+    }
 }
