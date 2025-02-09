1c0c7e4dcd0520903a89aca5453a94128f2521fd
Enya <enyaglf.2000@gmail.com>
2025-02-09 22:21:56 +0100

    ejercicio dragon ball terminado

--- 06-Asincronia/DragonBall/js/datosPersonaje.js
+++ 06-Asincronia/DragonBall/js/datosPersonaje.js
@@ -1,14 +1,13 @@
 window.onload = function () {
-    //Accedo al div para guardar ai la imagen
     let div = document.querySelector(".contenedor");
     let section = document.querySelector("section");
-    
+
 
     //Optengo la ide del personaje desde la url
     const urlParams = new URLSearchParams(window.location.search);
     const personajeId = urlParams.get("id"); // Obtener el id desde la URL
     //compruebo que la id existe
-    if(!personajeId){
+    if (!personajeId) {
         alert("No se ha encontrado la id del personaje");
         return;
 
@@ -17,7 +16,7 @@ window.onload = function () {
     //1º Necesito la url de la api
     const urlApi = `https://dragonball-api.com/api/characters/${personajeId}`; //aqui ya tengo las transformaciones
 
-   
+
     //✨---------------------FETCH PARA PERSONAJES-----------------✨
     fetch(urlApi)
         .then(function (respuesta) {
@@ -35,99 +34,127 @@ window.onload = function () {
         })
 
         .then(function (datos) {
-             //Titulo
-             let h1 = document.createElement("h1");
-             h1.textContent = datos.name;
 
-             div.appendChild(h1);
+            function CrearDatosPersonaje(datos) {
+                //Accedo al div para guardar ai la imagen
+                let div = document.querySelector(".contenedor");
+                //Titulo
+                let h1 = document.createElement("h1");
+                h1.textContent = datos.name;
 
-             //Creo el parrafo de info
-             let info = document.createElement("p");
-             info.className = "info-principal";
-             info.textContent = datos.description;
+                div.appendChild(h1);
 
-             div.appendChild(info);
+                //Creo el parrafo de info
+                let info = document.createElement("p");
+                info.classList.add("info-principal");
+                info.textContent = datos.description;
 
-             //Creo la imagen
-             let img = document.createElement("img");
-             img.src = datos.image;
-             div.appendChild(img);
+                div.appendChild(info);
 
-             //Titulo del planeta
-             let tituloPlaneta = document.createElement("h3");
-             tituloPlaneta.textContent = "🌍 El planeta de origen";
+                //Creo la imagen
+                let img = document.createElement("img");
+                img.src = datos.image;
+                div.appendChild(img);
 
-             div.appendChild(tituloPlaneta);
+                //Titulo del planeta
+                let tituloPlaneta = document.createElement("h3");
+                tituloPlaneta.textContent = "🌍 El planeta de origen";
+
+                div.appendChild(tituloPlaneta);
+            }
 
 
+            function CrearDetallesPlaneta(datos, div) {
 
-             for (const personaje_key in datos) {
-                if(personaje_key === "originPlanet"){
+                //Si no hay datos del planeta no muestro nada
+                if (!datos.originPlanet) return;
+                if (datos.originPlanet.name) {
                     //Creo el titulo del planeta de origen
                     let planeta = document.createElement("h3");
                     planeta.textContent = datos.originPlanet.name;
                     div.appendChild(planeta);
                 }
 
-                if(personaje_key === "description"){
+                if (datos.originPlanet.description) {
                     //Creo la descripcion del planeta de origen
                     let planetaDescripcion = document.createElement("p");
-                    planetaDescripcion.className = "planeta";
+                    planetaDescripcion.classList.add("planeta");
                     planetaDescripcion.textContent = datos.originPlanet.description;
                     div.appendChild(planetaDescripcion);
 
                 }
 
-                if(personaje_key === "image"){
+                if (datos.originPlanet.image) {
                     //Creo la descripcion del planeta de origen
                     let planetaImg = document.createElement("img");
                     planetaImg.src = datos.originPlanet.image;
                     div.appendChild(planetaImg);
                 }
 
-             }
-
-             //Aqui quiero crear las transformaciones
-             let tituloTransformacion = document.createElement("h3");
-             tituloTransformacion.textContent = "Transformaciones";
-             div.appendChild(tituloTransformacion);
+            }
 
 
-             for (const transformacion of datos.transformations) {
-                let aticulo = document.createElement("article");
 
-                //Creo el nombre
-                let nombreTransformacion = document.createElement("h2");
-                nombreTransformacion.textContent = transformacion.name;
-              
+            function CrearTransformaciones(datos, section) {
+                //Aqui quiero crear las transformaciones
+                let tituloTransformacion = document.createElement("h3");
+                tituloTransformacion.textContent = "Transformaciones";
+                div.appendChild(tituloTransformacion);
 
-                //Creo la imagen
-                let transformacionImg = document.createElement("img");
-                transformacionImg.src = transformacion.image;
-                
 
-                 //maximoqui
-                 let maxQi = document.createElement("p");
-                 maxQi.textContent = transformacion.ki;
-        
-                 
-                //titulo ki
-                let tituloKi = document.createElement("h3");
-                tituloKi.textContent = "Maximo Ki: ";
-                
+                for (const transformacion of datos.transformations) {
+                    let aticulo = document.createElement("article");
 
-                aticulo.appendChild(nombreTransformacion);
-                aticulo.appendChild(transformacionImg);
-                aticulo.appendChild(maxQi);
-                aticulo.appendChild(tituloKi);
+                    //Creo el nombre
+                    let nombreTransformacion = document.createElement("h2");
+                    nombreTransformacion.textContent = transformacion.name;
 
-                section.appendChild(aticulo);
-                
-             }
 
-             
-             
-      
+                    //Creo la imagen
+                    let transformacionImg = document.createElement("img");
+                    transformacionImg.src = transformacion.image;
+
+
+                    //maximoqui
+                    let maxQi = document.createElement("p");
+                    maxQi.textContent = transformacion.ki;
+
+
+                    //titulo ki
+                    let tituloKi = document.createElement("h3");
+                    tituloKi.textContent = "Maximo Ki: ";
+
+
+                    aticulo.appendChild(nombreTransformacion);
+                    aticulo.appendChild(transformacionImg);
+                    aticulo.appendChild(maxQi);
+                    aticulo.appendChild(tituloKi);
+
+                    section.appendChild(aticulo);
+
+                }
+            }
+
+            // Crear el botón
+            let boton = document.createElement("button");
+            boton.textContent = "Volver a personajes";
+            boton.onclick = function () {
+                window.location.href = "personajes.html";
+            };
+
+            // Añadir el botón al contenedor
+            let contenedor = document.getElementsByClassName("contenedor")[0]; 
+            contenedor.appendChild(boton);
+
+
+            //-------------------LLAMO A TODAS LAS FUNCIONES-------------------
+            //Llamo a la funcion para crear los datos del personaje
+            CrearDatosPersonaje(datos);
+            //Utilizo la funcion y creo los detalles del planeta -- arriba del todo tengo el div
+            CrearDetallesPlaneta(datos, div);
+            //Utilizo datos y el section
+            CrearTransformaciones(datos, section);
+
         })
         //coge el error y informa(nos coge todos los errores del codigo)
         .catch(function (error) {
--- 06-Asincronia/DragonBall/js/personajes.js
+++ 06-Asincronia/DragonBall/js/personajes.js
@@ -1,6 +1,6 @@
 window.onload = function () {
-        //Accedo al div para guardar ai la imagen
-        let section = document.querySelector("section");
+         //Accedo al div para guardar ai la imagen
+         let section = document.querySelector("section");
 
         //1º Necesito la url de la api
         const urlApi = "https://dragonball-api.com/api/characters";
@@ -26,54 +26,59 @@ window.onload = function () {
                 //Devuelvo en formato json
                 return respuesta.json();
             })
+            
 
             .then(function (datos) {
-            
-                for (const personaje of datos.items) {
 
-                    //Primero creo los elementos que necesito
-                    let article = document.createElement("article");
+                function CrearTarjetasPersonajes(personaje, section){
+                     //Primero creo los elementos que necesito
+                     let article = document.createElement("article");
 
-                    let h3Nombre = document.createElement("h3");
-                    //Añado el contenido al h3
-                    h3Nombre.textContent = personaje.name;
-
-                    let img = document.createElement("img");
-                    //le asigno atributro src a la imagen
-                    img.src = personaje.image;
-
-                    //Creo el h4 de la raza
-                    let h4Raza = document.createElement("h4");
-                    h4Raza.textContent = "Raza: " + personaje.race;
-
+                     let h3Nombre = document.createElement("h3");
+                     //Añado el contenido al h3
+                     h3Nombre.textContent = personaje.name;
+ 
+                     let img = document.createElement("img");
+                     //le asigno atributro src a la imagen
+                     img.src = personaje.image;
+ 
                      //Creo el h4 de la raza
-                     let h4MaxKi = document.createElement("h4");
-                     h4MaxKi.textContent ="Maximo Ki: " + personaje.maxKi;
-
-                    //añado el articulo al padre (section)
-                   
-                    article.appendChild(h3Nombre);
-                    article.appendChild(img);
-                    article.appendChild(h4Raza);
-                    article.appendChild(h4MaxKi);
-                    section.appendChild(article);
-
+                     let h4Raza = document.createElement("h4");
+                     h4Raza.textContent = "Raza: " + personaje.race;
+ 
+                      //Creo el h4 de la raza
+                      let h4MaxKi = document.createElement("h4");
+                      h4MaxKi.textContent ="Maximo Ki: " + personaje.maxKi;
+ 
+                     //añado el articulo al padre (section)
                     
+                     article.appendChild(h3Nombre);
+                     article.appendChild(img);
+                     article.appendChild(h4Raza);
+                     article.appendChild(h4MaxKi);
+                     section.appendChild(article);
+ 
+                     //Creo un listener para una accion que voy a realizar en el article
+                     article.addEventListener("click",function () {
+ 
+                         // Añadir el ID del personaje al artículo
+                         article.setAttribute("data-id", personaje.id);
+                         // Obtener el ID del personaje desde el atributo data-id del artículo
+                         let personajeId = article.getAttribute("data-id");
+ 
+                     // Redirigir a la página de detalles con el ID del personaje en la URL
+                     if(personajeId) {
+                         window.location.href = `datosPersonaje.html?id=${personajeId}`;
+                     }
+                         
+                     })
 
-                    //Creo un listener para una accion que voy a realizar en el article
-                    article.addEventListener("click",function () {
+                }
+               
 
-                        // Añadir el ID del personaje al artículo
-                        article.setAttribute("data-id", personaje.id);
-                        // Obtener el ID del personaje desde el atributo data-id del artículo
-                        let personajeId = article.getAttribute("data-id");
-
-                    // Redirigir a la página de detalles con el ID del personaje en la URL
-                    if(personajeId) {
-                        window.location.href = `datosPersonaje.html?id=${personajeId}`;
-                    }
-                        
-                    })
+                //Aqui llamo a la funcion
+                for (const personaje of datos.items) {
+                    CrearTarjetasPersonajes(personaje, section);
                 }
             })
             //coge el error y informa(nos coge todos los errores del codigo)
